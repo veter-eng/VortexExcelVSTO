b8ff3270c3390165f516d68e39c64a5237df44f9
Henrique Morais <rikeaju@hotmail.com>
2025-12-23 06:53:03 -0300

    chore(version): bump version to 1.1.0

--- VortexExcelAddIn/Properties/AssemblyInfo.cs
+++ VortexExcelAddIn/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Security;
 // É possível especificar todos os valores ou usar como padrão os Números da Versão e da Revisão 
 // utilizando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
