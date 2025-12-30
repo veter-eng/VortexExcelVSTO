1e6c7c0934530ac2a2b9fbf88de93b101b3c1ef7
Henrique Morais <rikeaju@hotmail.com>
2025-12-30 11:06:50 -0300

    refactor(ui): update ConfigPanel code-behind
    Add using statement for Domain.Models namespace.
    No functional changes, supports new multi-database UI.

--- VortexExcelAddIn/Views/ConfigPanel.xaml.cs
+++ VortexExcelAddIn/Views/ConfigPanel.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using VortexExcelAddIn.ViewModels;
 
 namespace VortexExcelAddIn.Views
 {
@@ -10,6 +12,19 @@ namespace VortexExcelAddIn.Views
         public ConfigPanel()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handler para mudanças na senha do PasswordBox.
+        /// PasswordBox não suporta binding direto por motivos de segurança,
+        /// então usamos este handler para atualizar o ViewModel.
+        /// </summary>
+        private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ConfigViewModel viewModel && sender is PasswordBox passwordBox)
+            {
+                viewModel.Password = passwordBox.Password;
+            }
         }
     }
 }
