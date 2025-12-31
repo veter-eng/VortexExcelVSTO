695e189c473ab673c1b4d1c2a39f4d311791a6b8
Henrique Morais <rikeaju@hotmail.com>
2025-12-31 15:29:15 -0300

    feat(ui): add auto-refresh status panel code-behind

---
+++ VortexExcelAddIn/Views/AutoRefreshStatusPanel.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace VortexExcelAddIn.Views
+{
+    /// <summary>
+    /// Interaction logic for AutoRefreshStatusPanel.xaml
+    /// </summary>
+    public partial class AutoRefreshStatusPanel : UserControl
+    {
+        public AutoRefreshStatusPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
